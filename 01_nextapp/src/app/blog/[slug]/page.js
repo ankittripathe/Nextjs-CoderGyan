062829ab2d385dafa9cc7725fc06a0c326ac51dc
Ankit Tripathi <ankittripathi2k24@gmail.com>
2025-12-02 13:26:15 +0530

    nextjs setup

--- 01_nextapp/src/app/blog/[slug]/page.js
+++ 01_nextapp/src/app/blog/[slug]/page.js
@@ -10,3 +10,4 @@ const page = async ({ params }) => {
 };
 
 export default page;
+
--- 01_nextapp/src/app/page.js
+++ 01_nextapp/src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-xl">Welcome to Nextjs Crash Course</h1>
+      <h1 className="text-xl">
+        Welcome to Nextjs Crash Course
+      </h1>
     </div>
   );
 }
