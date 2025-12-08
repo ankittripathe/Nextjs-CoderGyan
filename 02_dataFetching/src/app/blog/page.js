15bfce6931339f2fa7704a95a06b24a6c0cd2375
Ankit Tripathi <ankittripathi2k24@gmail.com>
2025-12-08 16:59:34 +0530

    data Fatching

--- 02_dataFetching/src/app/blog/page.js
+++ 02_dataFetching/src/app/blog/page.js
@@ -4,17 +4,19 @@ const BlogPage = async () => {
   const data = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await data.json();
   // console.log(posts);
-  
-  return (
-    <div>
-      <h1>Blog Posts</h1>
 
-      <div className="flex flex-wrap gap-5">
+  return (
+    <div className="px-6">
+      <h1>Blog Posts......</h1>
+
+      <div className="flex flex-wrap gap-5 ">
         {posts.map((mypost) => {
           return (
             <div key={mypost.id} className="border p-3">
               <Link href={`/blog/${mypost.id}`}>
-                <h2 className="text-indigo-500">{mypost.id}: {mypost.title}</h2>
+                <h2 className="text-indigo-500">
+                  {mypost.id}: {mypost.title}
+                </h2>
               </Link>
               <p>{mypost.body}</p>
             </div>
